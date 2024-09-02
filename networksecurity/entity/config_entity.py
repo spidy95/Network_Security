65a57dde9eb6e0c52297c630f33937771ee5a0af
spidy95 <atul.spider95@gmail.com>
2024-09-02 11:10:23 +0530

    data transformation

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -51,7 +51,7 @@ class DataValidationConfig:
 
 
 class DataTransformationConfig:
-    def __init__(self):
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_transformation_dir: str = os.path.join( training_pipeline_config.artifact_dir,training_pipeline.DATA_TRANSFORMATION_DIR_NAME )
         self.transformed_train_file_path: str = os.path.join( self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
             training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"),)
